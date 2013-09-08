e7f4f5383deb50023ec415e71d7a8a37b721b57f
François Massot <francois.massot@gmail.com>
2013-09-08 19:42:59 +0200

    Make spider work for all DOMs. Reorganize it to crawl also departments, regions and epci.

--- localgouv/spiders/localgouv_spider.py
+++ localgouv/spiders/localgouv_spider.py
@@ -20,12 +20,36 @@ class LocalGouvFinanceSpider(BaseSpider):
         - exercise: year of financial data
         """
     name = "localgouv"
-    allowed_domains = ["http://alize2.finances.gouv.fr"]
+    domain = "http://alize2.finances.gouv.fr"
+    allowed_domains = [domain]
 
-    def __init__(self, insee_code_file="./data/france2013.txt", year=2012):
+    def __init__(self, year=2012, com=True, depreg=False, epci=False):
         """Load insee code of every commune in france and generate all the urls to
         crawl."""
+        self.start_urls = []
+        if com:
+            self.start_urls += self.get_commune_urls(year)
+        if depreg:
+            self.start_urls += self.get_depreg_urls(year)
+        if epci:
+            self.start_urls += self.get_epci_urls(year)
 
+    def get_dep_and_region_urls(self, year):
+        #TODO
+        dep_baseurl = "%s/departements/detail.php?dep=%%(dep)s&exercice=%s"%(self.domain, year)
+        reg_baseurl = "%s/regions/detail.php?reg=%%(reg)s&exercice=%s"%(self.domain, year)
+        return []
+
+    def get_epci_urls(self, year):
+        xls = pd.ExcelFile('./data/epci-au-01-01-2013.xls')
+        data = xls.parse('Composition communale des EPCI')
+        data['siren'] = df[u'Établissement public à fiscalité propre'][1:]
+        data['dep'] = df[u'Département commune'].apply(lambda r: ('0%s'%r)[:3])
+        baseurl = "%s/communes/eneuro/detail_gfp.php?siren=%%(siren)s&dep=%%(dep)s&type=BPS&exercice=%s"%(self.domain, str(year))
+        return [baseurl%row for __, row in data.iterrows()][1:]
+
+    def get_commune_urls(self, year):
+        insee_code_file="./data/france2013.txt"
         # XXX: insee_communes file contains also "cantons", filter out these lines
         # XXX: some departments are not crawled correctly: 75, 92, 93, 94 and maybe
         # others. Fix this.
@@ -53,19 +77,34 @@ class LocalGouvFinanceSpider(BaseSpider):
                 '974': '104',
             }.get(code, code)
         data['DEP'] = data['DEP'].apply(convert_dep)
-        # Finally, DOM cities have an insee_code on 2 digits in the insee file. We need
-        # to add a third digit before these two to crawl the right page.
+        # Another strange thing, DOM cities have an insee_code on 2 digits in the
+        # insee file. We need to add a third digit before these two to crawl the
+        # right page. This third digit is find according to this mapping:
+        # GUADELOUPE: 1
+        # MARTINIQUE: 2
+        # GUYANE: 3
+        # REUNION: 4
+        digit_mapping = {'101': 1, '103': 2, '102': 3, '104': 4}
         def convert_city(row):
-            if row['DEP'] in ['101', '102', '103', '104']:
-                return row['DEP'][-1] + row['COM'][1:]
-            else:
+            if row['DEP'] not in ['101', '102', '103', '104']:
                 return row['COM']
+            first_digit = str(digit_mapping.get(row['DEP']))
+            return first_digit + row['COM'][1:]
         data['COM'] = data.apply(convert_city, axis=1)
 
-        baseurl = "http://alize2.finances.gouv.fr/communes/eneuro/detail.php?icom=%(COM)s&dep=%(DEP)s&type=BPS&param=0&exercice=" + str(year)
-        self.start_urls = [baseurl%row for __, row in data.iterrows()]
+        baseurl = "%s/communes/eneuro/detail.php?icom=%%(COM)s&dep=%%(DEP)s&type=BPS&param=0&exercice=%s"%(self.domain,str(year))
+        return [baseurl%row for __, row in data.iterrows()]
 
     def parse(self, response):
+        if "/communes/eneuro/detail_gf.php" in response.url:
+            return self.parse_epci(response)
+        elif "/communes/eneuro/detail.php" in response.url:
+            return self.parse_commune(response)
+        elif "/departements/detail.php" in response.url or \
+                "/regions/detail.php" in reponse.url:
+            self.parse_dep_and_reg(response)
+
+    def parse_commune(self, response):
         """Parse the response and return an Account object"""
         hxs = HtmlXPathSelector(response)
         icom, dep, year = re.search('icom=(\d{3})&dep=(\w{3})&type=\w{3}&param=0&exercice=(\d{4})', response.url).groups()
@@ -79,5 +118,11 @@ class LocalGouvFinanceSpider(BaseSpider):
             data = {'code': icom+dep, 'year': year, 'is_city': True, 'url': response.url}
             log.msg('failed url %s'%response.url, level=log.ERROR)
             raise
+
+    def parse_epci(self, response):
+        hxs = HtmlXPathSelector(response)
+
+    def parse_dep_and_reg(self, response):
+        hxs = HtmlXPathSelector(response)
 
 
