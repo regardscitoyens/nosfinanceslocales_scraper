e508803a1e57e310aca43b605c952422ab0c7203
François Massot <francois.massot@gmail.com>
2013-09-08 19:40:39 +0200

    Make the test work again.

--- test/test_commune_scraping.py
+++ test/test_commune_scraping.py
@@ -12,18 +12,23 @@ from scrapy.http.response.html import HtmlResponse
 from localgouv.account_network import city_account
 from localgouv.account_parsing import parse_city_page_account
 
+
+def get_reponse(filepath):
+    body = open(filepath, 'r').read()
+    response = HtmlResponse('test', encoding='utf-8')
+    response.body = body
+    return response
+
 class CommuneFinanceParsingTestCase(unittest2.TestCase):
     def setUp(self):
-        body = open('data/test_commune_body.html', 'r').read()
-        self.response = HtmlResponse('test', encoding='utf-8')
-        self.response.body = body
+        self.response = get_response('data/commune_2012_account.html')
 
     def test_parsing(self):
         data = parse_city_page_account("", "", "2012", self.response)
         # test data parsed from first table
-        self.assertEqual(data['operating_revenues']['value'], 210000.)
-        self.assertEqual(data['localtax']['value_per_person'], 289.)
-        self.assertEqual(data['operating_costs']['value_per_person'], 542.)
+        self.assertEqual(data['operating_revenues'], 210000.)
+        self.assertEqual(data['localtax'], 289.)
+        self.assertEqual(data['operating_costs'], 542.)
 
         # test data parsed from second table
         self.assertEqual(data['home_tax']['value'], 47000.)
