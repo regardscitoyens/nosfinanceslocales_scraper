4f24bd7b23402681647cd2d82ccd1f923c18f62f
fmassot <francois.massot@gmail.com>
2017-02-04 00:41:53 +0100

    Update script to retrieve all epci

--- bin/create_epci_csv.sh
+++ bin/create_epci_csv.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
 
-year=2012
+year=2008
 
 mkdir -p .cache
 cd .cache
 
-for year in $(seq $year 2014);
+for year in $(seq $year 2015);
 do
-    wget -N http://www.insee.fr/fr/methodes/zonages/epci-au-01-01-${year}.zip
-    rm -f epci-au-01-01-${year}.xls
-    unzip epci-au-01-01-${year}.zip
+    wget -N "http://www.collectivites-locales.gouv.fr/files/files/epcicom${year}.csv"
 done
 
 cd ..
 
-python bin/create_epci_csv.py
